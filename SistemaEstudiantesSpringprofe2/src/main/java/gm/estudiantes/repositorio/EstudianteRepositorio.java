72618999fe835b0febd33138b9ff4475f78b2ebe
Daniel Arias Code <daniel.arias.sarmiento@gmail.com>
2024-01-27 02:25:20 -0500

    modificacion de commit

--- SistemaEstudiantesSpringprofe2/src/main/java/gm/estudiantes/repositorio/EstudianteRepositorio.java
+++
@@ -1,7 +0,0 @@
-package gm.estudiantes.repositorio;
-
-import gm.estudiantes.modelo.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
-}
--- tienda_libros-profe2/src/main/java/gm/tienda_libros/repositorio/LibroRepositorio.java
+++
@@ -1,7 +0,0 @@
-package gm.tienda_libros.repositorio;
-
-import gm.tienda_libros.modelo.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepositorio extends JpaRepository<Libro, Integer> {
-}
